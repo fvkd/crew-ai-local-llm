17a99fa3251fd7e6c1288f980d39c359315d4b5a
vi|vi|vi <fvcker@skiff.com>
2024-03-29 05:23:07 -0400

    Update create-llama2-model-file.ps1

--- setup/create-llama2-model-file.ps1
+++ setup/create-llama2-model-file.ps1
@@ -1,7 +1,9 @@
 # Variables
 $model_name = "llama2"
 $custom_model_name = "crewai-llama2"
+
 # Get the base model
 ollama pull $model_name
+
 # Create the model file
 ollama create $custom_model_name -f ./Llama2Modelfile
